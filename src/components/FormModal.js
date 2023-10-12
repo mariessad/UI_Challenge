360db9fa14370cdcb5a54aa24f8cd18ab6e911f8
mariessad <74148632+mariessad@users.noreply.github.com>
2023-10-12 15:48:10 -0400

    edit form modal and add form modal css

--- src/components/FormModal.js
+++ src/components/FormModal.js
@@ -1,22 +1,24 @@
 import React, {useState} from 'react';
 import "./FormModal.css";
 
-const FormModal = () => {
-  const handleFormModalSubmit = (e) => {
-    e.preventDefault();
+const FormModal = ({submit}) => {
+//   const handleFormModalSubmit = (e) => {
+//     e.preventDefault();
 
-    const body = {
-      firstName: e.target.firstName.value,
-      lastName: e.target.lastName.value,
-      dateOfBirth: e.target.dateOfBirth.value,
-      phoneNumber: e.target.phoneNumber.value,
-      address: e.target.address.value,
-      notes: e.target.notes.value,
-    };
+//     const body = {
+//       firstName: e.target.firstName.value,
+//       lastName: e.target.lastName.value,
+//       dateOfBirth: e.target.dateOfBirth.value,
+//       phoneNumber: e.target.phoneNumber.value,
+//       address: e.target.address.value,
+//       notes: e.target.notes.value,
+//     };
     return (
       <div className='modal-container'>
+      <div className='modal'> 
         <h3>Complete the fields below.</h3>
-        <form onSubmit={handleFormModalSubmit}>
+        <span>Optional*</span>
+        <form onSubmit={submit}>
           <label htmlFor="firstName">First Name</label>
           <input type="text" name="firstName" id="firstName" required/>
           <label htmlFor="lastName">Last Name</label>
@@ -26,13 +28,16 @@ const FormModal = () => {
           <input type="text" name="dateOfBirth" id="dateOfBirth"  required/>
           <label htmlFor="address">Address</label>
           <input type="text" name="address" id="address" required/>
-          <label htmlFor="notes">Notes</label>
+          <label htmlFor="notes">Notes*</label>
           <textarea name="notes" id="notes"></textarea>
+          <div className='button-container'>
           <button type="">Cancel</button>
           <button type="submit">Submit</button>
+          </div>
         </form>
+        </div>
       </div>
     );
   };
-};
+
 export default FormModal;
